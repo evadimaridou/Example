170978916fa9aa690eeec0823966b4e8b4832f1b
evadimaridou <evadimaridou@gmail.com>
2025-02-02 22:09:25 +0200

    Added comments

--- controllers/Default.js
+++ controllers/Default.js
@@ -4,6 +4,7 @@ var utils = require('../utils/writer.js');
 var Default = require('../service/DefaultService');
 
 // DELETE an author with specific author id
+// Calls the service folder function
 module.exports.authorsAuthorIdDELETE = function authorsAuthorIdDELETE (_, res, __, authorId) {
   Default.authorsAuthorIdDELETE(authorId)
     .then(function (response) {
@@ -15,6 +16,7 @@ module.exports.authorsAuthorIdDELETE = function authorsAuthorIdDELETE (_, res, _
 };
 
 // GET an author with specific author id
+// Calls the service folder function
 module.exports.authorsAuthorIdGET = function authorsAuthorIdGET (_, res, __, authorId) {
   Default.authorsAuthorIdGET(authorId)
     .then(function (response) {
@@ -26,6 +28,7 @@ module.exports.authorsAuthorIdGET = function authorsAuthorIdGET (_, res, __, aut
 };
 
 // EDIT an author with specific author id
+// Calls the service folder function
 module.exports.authorsAuthorIdPUT = function authorsAuthorIdPUT (_, res, __, body, authorId) {
   Default.authorsAuthorIdPUT(body, authorId)
     .then(function (response) {
@@ -37,6 +40,7 @@ module.exports.authorsAuthorIdPUT = function authorsAuthorIdPUT (_, res, __, bod
 };
 
 // GET all authors
+// Calls the service folder function
 module.exports.authorsGET = function authorsGET (_, res, __) {
   Default.authorsGET()
     .then(function (response) {
@@ -48,6 +52,7 @@ module.exports.authorsGET = function authorsGET (_, res, __) {
 };
 
 // CREATE an author 
+// Calls the service folder function
 module.exports.authorsPOST = function authorsPOST (_, res, __, body) {
   Default.authorsPOST(body)
     .then(function (response) {
@@ -58,6 +63,7 @@ module.exports.authorsPOST = function authorsPOST (_, res, __, body) {
     });
 };
 // DELETE a book with specific book id
+// Calls the service folder function
 module.exports.booksBookIdDELETE = function booksBookIdDELETE (_, res, __, bookId) {
   Default.booksBookIdDELETE(bookId)
     .then(function (response) {
@@ -69,6 +75,7 @@ module.exports.booksBookIdDELETE = function booksBookIdDELETE (_, res, __, bookI
 };
 
 // GET a book with specific book id
+// Calls the service folder function
 module.exports.booksBookIdGET = function booksBookIdGET (_, res, __, bookId) {
   Default.booksBookIdGET(bookId)
     .then(function (response) {
@@ -80,6 +87,7 @@ module.exports.booksBookIdGET = function booksBookIdGET (_, res, __, bookId) {
 };
 
 // EDIT a book with specific book id
+// Calls the service folder function
 module.exports.booksBookIdPUT = function booksBookIdPUT (_, res, __, body, bookId) {
   Default.booksBookIdPUT(body, bookId)
     .then(function (response) {
@@ -90,6 +98,7 @@ module.exports.booksBookIdPUT = function booksBookIdPUT (_, res, __, body, bookI
     });
 };
 // GET all books 
+// Calls the service folder function
 module.exports.booksGET = function booksGET (_, res, __) {
   Default.booksGET()
     .then(function (response) {
@@ -101,6 +110,7 @@ module.exports.booksGET = function booksGET (_, res, __) {
 };
 
 // CREATE a book 
+// Calls the service folder function
 module.exports.booksPOST = function booksPOST (_, res, __, body) {
   Default.booksPOST(body)
     .then(function (response) {
@@ -112,6 +122,7 @@ module.exports.booksPOST = function booksPOST (_, res, __, body) {
 };
 
 // DELETE a category with specific category id
+// Calls the service folder function
 module.exports.categoriesCategoryIdDELETE = function categoriesCategoryIdDELETE (_, res, __, categoryId) {
   Default.categoriesCategoryIdDELETE(categoryId)
     .then(function (response) {
@@ -123,6 +134,7 @@ module.exports.categoriesCategoryIdDELETE = function categoriesCategoryIdDELETE 
 };
 
 // GET a category with specific category id
+// Calls the service folder function
 module.exports.categoriesCategoryIdGET = function categoriesCategoryIdGET (_, res, __, categoryId) {
   Default.categoriesCategoryIdGET(categoryId)
     .then(function (response) {
@@ -134,6 +146,7 @@ module.exports.categoriesCategoryIdGET = function categoriesCategoryIdGET (_, re
 };
 
 // EDIT a category with specific category id
+// Calls the service folder function
 module.exports.categoriesCategoryIdPUT = function categoriesCategoryIdPUT (_, res, __, body, categoryId) {
   Default.categoriesCategoryIdPUT(body, categoryId)
     .then(function (response) {
@@ -145,6 +158,7 @@ module.exports.categoriesCategoryIdPUT = function categoriesCategoryIdPUT (_, re
 };
 
 // GET all categories 
+// Calls the service folder function
 module.exports.categoriesGET = function categoriesGET (_, res, __) {
   Default.categoriesGET()
     .then(function (response) {
@@ -156,6 +170,7 @@ module.exports.categoriesGET = function categoriesGET (_, res, __) {
 };
 
 // CREATE a new category
+// Calls the service folder function
 module.exports.categoriesPOST = function categoriesPOST (_, res, __, body) {
   Default.categoriesPOST(body)
     .then(function (response) {
