bc93b8703cc2506d02635d7a185f95a70a79b050
evadimaridou <evadimaridou@gmail.com>
2025-02-02 13:08:01 +0200

    [MODIFIED]: Added comments in writer.js

--- utils/writer.js
+++ utils/writer.js
@@ -1,12 +1,15 @@
+//ResponsePayload object
 var ResponsePayload = function(code, payload) {
   this.code = code;
   this.payload = payload;
 }
 
+// Creates ResponsePayload object
 exports.respondWithCode = function(code, payload) {
   return new ResponsePayload(code, payload);
 }
 
+//Creates json object
 var writeJson = exports.writeJson = function(response, arg1, arg2) {
   var code;
   var payload;
@@ -15,14 +18,14 @@ var writeJson = exports.writeJson = function(response, arg1, arg2) {
     writeJson(response, arg1.payload, arg1.code);
     return;
   }
-
+  //Choose code from arg2 or arg1
   if(arg2 && Number.isInteger(arg2)) {
     code = arg2;
   }
   else if (arg1 && Number.isInteger(arg1)) {
       code = arg1;
     }
-  
+  //Create payload
   if(code && arg1) {
     payload = arg1;
   }
@@ -37,6 +40,7 @@ var writeJson = exports.writeJson = function(response, arg1, arg2) {
   if(typeof payload === 'object') {
     payload = JSON.stringify(payload, null, 2);
   }
+  //Write response
   response.writeHead(code, {'Content-Type': 'application/json'});
   response.end(payload);
 }
